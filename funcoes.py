6a75ff6fe13f8b3a77685c0d480120539fcb2e41
malu-zinha <maluchoa07@gmail.com>
2026-09-14 13:59:05 -0300

    new: documentação

---
+++ funcoes.py
@@ -0,0 +1,21 @@
+# Algumas funções e sintaxe que eu vivo esquecendo rs <3
+
+s = []
+s = s[0:10] # corta a string
+s = s[::-1] # inverte a string
+
+s = s.replace("substituido", "substituicao") # substitui algo na string
+s = s.lower() # tudo minúsculo
+s = s.upper() # tudo maiúsculo
+
+# LISTA:
+
+lista = []
+lista.append(n)
+len(lista) # tamanho da lista
+range(len(s)) # range do tamanho da lista
+
+# HASH:
+
+tabela = set()
+tabela.add()
